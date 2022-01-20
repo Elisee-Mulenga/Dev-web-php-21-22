1d0334df248dfc5ce7ee02001af730d61a9aed4b
Elisée MULENGA MULAPU <19mm345@esisalama.org>
2022-01-20 22:56:51 +0100

    First modif

--- Index.php
+++ Index.php
@@ -8,8 +8,8 @@
 </head>
 <body>
     <form>
-        Nom : <input type= "text" name="le nom"> <br>
-        post-nom : <input type= "text" name="pst nom"> <br>
+        <label>Nom</label> : <input type="text" name="nom" />
+        <label>Post-nom</label> : <input type="text" name="pst nom" /><br>
         Prenom : <input type= "text" name="prenom"> <br>
         Matricule : <input type= "text" name="mat"> <br>
         Genre : <input type= "text" name="sexe"> <br>
@@ -17,9 +17,7 @@
         Email : <input type= "text" name="adresse"> <br>
         <button type="submit">Submit</button>
         <form method="post" action="traitement.php">
-        <p>
-            <label>Votre pseudo</label> : <input type="text" name="pseudo" id="pseudo" />
-        </p>
+
     </form>
 </body>
 </html>
