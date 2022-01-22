0b65b74eaad3f9ecaabd4fcacdf818ababeb7884
Elisée MULENGA MULAPU <19mm345@esisalama.org>
2022-01-22 23:04:53 +0100

    Verification et affichage des variables

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -7,13 +7,27 @@
         // echo $name;
     // verifications des données
 
-        if (isset ($_POST['name'] ) ) {
+        if (isset ($_POST['name'])) {
             $name = $_POST['name'];
-            echo "Bonjour Mr.".$name;
-        } else {
+            echo "NOM : ".$name ."<br>"; }
+        if (isset ($_POST['lastname'])) {
+            $lastname = $_POST['lastname'];
+            echo "POST-NOM : ".$lastname ."<br>"; }
+        if (isset ($_POST['nickname'])) {
+            $nickname = $_POST['nickname'];
+            echo "PRENOM : ".$nickname ."<br>"; }
+        if (isset ($_POST['idname'])) {
+            $idname = $_POST['idname'];
+            echo "MATRICULE : ".$idname ."<br>"; }
+        if (isset ($_POST['promo'])) {
+            $promo = $_POST['promo'];
+            echo "PROMOTION : ".$promo ."<br>"; }
+        if (isset ($_POST['mail'])) {
+            $mail = $_POST['mail'];
+            echo "Mail : ".$mail ."<br>"; }
+        else {
             echo "Désolé cette clé n'existe pas !";
         }
     // la fonction issert () : permet de verifier si une valeur possede une clé
-
-
 ?>
+
