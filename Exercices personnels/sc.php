77933c0e6b658f35c7d59173d328d15d1ace6127
Elisée MULENGA MULAPU <19mm345@esisalama.org>
2022-01-23 22:52:40 +0100

    dernier test sur le code

---
+++ Exercices personnels/sc.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>Elisee - Php cours</title>
+    </head>
+
+    <body>
+        <?php
+        echo "Hello world, ESIS une ecole un metier !";
+        ?>
+
+        <?php
+            // On crée notre array $coordonnees
+            $coordonnees = array (
+            'prenom' => 'François',
+            'nom' => 'Elisée MULENGA',
+            'adresse' => '3 Rue du Paradis',
+            'ville' => 'Paris ma ville adorée');
+            echo 'ville';
+            echo 'nom';
+            echo 'prenom';
+        ?>
+
+    </body>
+</html>
+
